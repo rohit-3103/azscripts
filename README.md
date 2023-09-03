0074c26ec7ce9edbd9c3dfeea5a2617631c0a0ef
Rohit <rohit-naik@live.com>
2023-09-04 00:38:39 +0530

    Saving the commands file

---
+++ README.md
@@ -0,0 +1,2 @@
+# This file is created as Cheat sheet for Az commands
+# Author : Rohit Naik
---
+++ azcommands.ps1
@@ -0,0 +1,81 @@
+# get the powershell version on your machine
+$psversiontable
+
+# install module
+install-module az -Scope AllUsers -Force #while using mac it gives error so just installed for current user
+
+install-module az
+
+#get version details
+Get-Module az -ListAvailable
+
+#Update the module using below commands
+Update-Module az -Scope AllUsers
+update-module az
+#NOTE old versions may still be present so try to delete and install the module again
+
+#login to Azure
+Connect-AzAccount
+
+#Connect to Azure using organizational ID credentials
+$Credential = Get-Credential
+Connect-AzAccount -Credential $Credential
+
+
+get-azcontext -ListAvailable
+
+#rename Subscription
+$contexts= get-azcontext -ListAvailable
+$contexts[1] | rename-azcontext -TargetName "Rohit Dev"
+$contexts[2] | rename-azcontext -TargetName "Rohit Lab"
+
+Select-AzContext "Rohit Dev"
+
+#search commands using module , their verb and their Noun
+get-command -module AZ.Compute
+# CommandType     Name                                               Version    Source
+# -----------     ----                                               -------    ------
+# Alias           Get-AzVmssDiskEncryptionStatus                     6.2.0      Az.Compute
+# Alias           Get-AzVmssVMDiskEncryptionStatus                   6.2.0      Az.Compute
+# Alias           Invoke-AzVMAssessPatch                             6.2.0      Az.Compute
+# Alias           Invoke-AzVMPatchAssess                             6.2.0      Az.Compute
+# Alias           Repair-AzVmssServiceFabricUD                       6.2.0      Az.Compute
+
+get-command -module AZ.Compute -Verb New
+# CommandType     Name                                               Version    Source
+# -----------     ----                                               -------    ------
+# Function        New-AzGalleryApplication                           6.2.0      Az.Compute
+# Function        New-AzGalleryApplicationVersion                    6.2.0      Az.Compute
+# Cmdlet          New-AzAvailabilitySet                              6.2.0      Az.Compute
+# Cmdlet          New-AzCapacityReservation                          6.2.0      Az.Compute
+
+get-command -module AZ.Compute -Noun AZVM 
+# CommandType     Name                                               Version    Source
+# -----------     ----                                               -------    ------
+# Cmdlet          Get-AzVM                                           6.2.0      Az.Compute
+# Cmdlet          New-AzVM                                           6.2.0      Az.Compute
+# Cmdlet          Remove-AzVM                                        6.2.0      Az.Compute
+# Cmdlet          Restart-AzVM                                       6.2.0      Az.Compute
+# Cmdlet          Set-AzVM                                           6.2.0      Az.Compute
+# Cmdlet          Start-AzVM                                         6.2.0      Az.Compute
+# Cmdlet          Stop-AzVM                                          6.2.0      Az.Compute
+# Cmdlet          Update-AzVM                                        6.2.0      Az.Compute
+
+Get-AzResourceGroup
+#create Resource Group
+New-AzResourceGroup -Name 'myResourceGroup' -Location 'EastUS'
+
+#creating new VM in existing Resource Group
+New-AzVm `
+    -ResourceGroupName 'myResourceGroup' `
+    -Name 'myVM' `
+    -Location 'East US' `
+    -Image 'MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:latest' `
+    -VirtualNetworkName 'myVnet' `
+    -SubnetName 'mySubnet' `
+    -SecurityGroupName 'myNetworkSecurityGroup' `
+    -PublicIpAddressName 'myPublicIpAddress' `
+    -OpenPorts 80,3389
+
+#It will ask for Admin and password
+#op: No Size value has been provided. The VM will be created with the default size Standard_D2s_v3.
